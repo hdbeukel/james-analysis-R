0eca12b9a7355305fa7ec8f65443af44dcfe529f
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2015-03-13 10:48:51 +0100

    extended package doc

--- R/package.R
+++ R/package.R
@@ -16,10 +16,38 @@
 #' This version is written for use in combination with version 0.2.1 of the JAMES
 #' extensions module.
 #' 
+#' @examples
+#' # load example data
+#' data(james)
+#' summary(james)
+#' 
+#' # plot convergence curves for coconut data set
+#' plotConvergence(james, problem = "coconut", min.time = 1000, max.time = 100000)
+#' 
+#' # create box plots of solution values (quality) and convergence times
+#' boxplot(james, problem = "coconut")
+#' boxplot(james, problem = "coconut", type = "time")
+#' 
+#' # extract solution values and convergence times for parallel tempering and random descent
+#' values.pt <- getBestSolutionValues(james, problem = "coconut", search = "Parallel Tempering")
+#' times.pt <- getConvergenceTimes(james, problem = "coconut", search = "Parallel Tempering")
+#' values.rd <- getBestSolutionValues(james, problem = "coconut", search = "Random Descent")
+#' times.rd <- getConvergenceTimes(james, problem = "coconut", search = "Random Descent")
+#' 
+#' # perform wilcoxon test to compare distributions across algorithms
+#' values.test <- wilcox.test(values.pt, values.rd)
+#' values.test
+#' times.test <- wilcox.test(times.pt, times.rd)
+#' times.test
+#' 
+#' # adjust p-values for multiple testing
+#' p.adjust(c(values.test$p.value, times.test$p.value))
+#' 
 #' @section Example data: \code{\link{james}}
 #' 
 #' @section Data manipulation functions:
 #'   \itemize{
+#'    \item{\code{\link{readJAMES}}}
 #'    \item{\code{\link{reduceJAMES}}}
 #'    \item{\code{\link{mergeJAMES}}}
 #'    \item{\code{\link{getProblems}}}
